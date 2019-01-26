be46664d05e18af6f942d905d3f7a94dc6e081db
Mikolaj Przybylski <clovers02123@gmail.com>
2019-01-26 16:44:47 +0100

    teasdgf

--- GameJam/Assets/Scripts/ItemScore.cs
+++ GameJam/Assets/Scripts/ItemScore.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ItemScore : HouseState
 {
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (gameObject.name == "Player1_ItemLocation")
