3cb1faf14b47ea1e8ad958eaacedf26a9924c741
MikeMG <michalek.mg@gmail.com>
2019-01-26 03:03:29 +0100

    Revert "taktyczny commit"
    This reverts commit e11586025b2d40af0853aace71b537a2a0a0884f, reversing
    changes made to 0c1a22a1f750ea290c6789a946ed093e3d843219.

--- GameJam/Assets/Scripts/Item.cs
+++ GameJam/Assets/Scripts/Item.cs
@@ -1,27 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    public static GameObject collidingItem;
-    public static GameObject pickedItem;
+    public static bool itemColliding;
 
-    public static bool isColliding;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            isColliding = true;
-            collidingItem = gameObject;
-        }
+        Destroy(collision.gameObject);      
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            isColliding = false;
-            collidingItem = null;
+            itemColliding = false;
         }
     }
 }
---
+++ GameJam/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    void ItemPickUp()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (Item.itemColliding)
+            {
+                Debug.Log("Item podniesiony");
+            }
+        }
+    }
+
+    private void Update()
+    {
+        ItemPickUp();
+    }
+}
