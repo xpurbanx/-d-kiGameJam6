a036d671f1d83c4f88a5d429bff0e906f926866b
Mikolaj Przybylski <clovers02123@gmail.com>
2019-01-26 01:20:59 +0100

    Item script

---
+++ GameJam/Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public static GameObject collidingItem;
+    public static GameObject pickedItem;
+
+    public static bool isColliding;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isColliding = true;
+            collidingItem = gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isColliding = false;
+            collidingItem = null;
+        }
+    }
+}
