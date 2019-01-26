3b8c51b17e997191d0f7fd8b7b2433c039feb211
Purban <xpurbanx@wp.pl>
2019-01-26 18:13:17 +0100

    Static ItemRequired

--- GameJam/Assets/Scripts/HouseState.cs
+++ GameJam/Assets/Scripts/HouseState.cs
@@ -10,7 +10,7 @@ public class HouseState : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ItemRequired.Instance.printLists();
     }
 
     // Update is called once per frame
