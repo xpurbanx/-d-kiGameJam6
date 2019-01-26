83c852fda071f39d346f476a212643f70b8424fd
Purban <xpurbanx@wp.pl>
2019-01-26 18:14:45 +0100

    Revert "Static ItemRequired"
    This reverts commit 3b8c51b17e997191d0f7fd8b7b2433c039feb211.

--- GameJam/Assets/Scripts/ItemRequired.cs
+++ GameJam/Assets/Scripts/ItemRequired.cs
@@ -1,55 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class ItemRequired : MonoBehaviour
 {
-    public static ItemRequired Instance;
+    List<List<string>> itemsForLevel;
 
-    public List<List<string>> itemsForLevel;
-    public List<GameObject> allItems;
-    private List<GameObject> shuffledList = new List<GameObject>();
-    private List<GameObject> itemsSide1 = new List<GameObject>();
-    private List<GameObject> itemsSide2 = new List<GameObject>();
-
-
-    private void Awake()
+    //List<string> 
+    // Start is called before the first frame update
+    void Start()
     {
-        Instance = this;
-    }
-    private void Start()
-    {
-
-        shuffledList = allItems.OrderBy(x => Random.value).ToList();
-        splitList();
-        //printLists();
-
-    } 
-    private void splitList()
-    {
-        for (int i = 0; i < shuffledList.Count; i++) // as long as allItems.count % 2 == 0
-        {
-            itemsSide1.Add(shuffledList[i]);
-            i++;
-            itemsSide2.Add(shuffledList[i]);
-        }
-
-    }
-    public void printLists()
-    {
-        Debug.Log("LIST 1");
-        foreach (GameObject gameObject in itemsSide1)
-        {
-            Debug.Log(gameObject.name);
-        }
-        Debug.Log("LIST 2");
-        foreach (GameObject gameObject in itemsSide2)
-        {
-            Debug.Log(gameObject.name);
-        }
+        
     }
 
-
-
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
