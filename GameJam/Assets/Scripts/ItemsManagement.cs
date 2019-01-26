73a7f4952ab7ab31ca2713f3637ebfa040b1b6b3
Mikolaj Przybylski <clovers02123@gmail.com>
2019-01-26 02:14:20 +0100

    Prefaby itemów

--- GameJam/Assets/Scripts/ItemsManagement.cs
+++ GameJam/Assets/Scripts/ItemsManagement.cs
@@ -5,6 +5,7 @@ public class ItemsManagement : MonoBehaviour
     public GameObject[] itemType;
     public static bool isPicked;
 
+    private Vector2 lastPosition = new Vector3(0, 0, 0);
     private float mapX;
     private float mapY;
 
@@ -18,10 +19,11 @@ public class ItemsManagement : MonoBehaviour
         int randomItemType;
         for (int i = 0; i < 16; i++)
         {
-            randomItemType = Random.Range(0, 3);
+            randomItemType = Random.Range(0, 11);
             float x = Random.Range(-7.0f, 7.0f);
             float y = Random.Range(-7.0f, 7.0f);
             GameObject item = Instantiate(itemType[randomItemType]);
+
             item.transform.position = new Vector2(x, y);
         }
     }
--- GameJam/Assets/Scripts/PlayerActions.cs
+++ GameJam/Assets/Scripts/PlayerActions.cs
@@ -2,7 +2,7 @@
 
 public class PlayerActions : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 1.5f, -1f);
+    private Vector3 offset = new Vector3(0f, 2.0f, -1f);
     private Vector3 dropItem = new Vector3(0f, 1.5f, 0f);
     private float timePassed = 0f;
     private readonly float keyDelay = 0.2f;
@@ -29,6 +29,8 @@ public class PlayerActions : MonoBehaviour
                 Item.pickedItem.transform.position = GameObject.FindGameObjectWithTag("Player").transform.position - dropItem;
 
                 Item.pickedItem = null;
+                
+                if (gameObject.GetComponent<Collider2D>().IsTouching(Collider2D collider.gameObject.tag == "Item"))
                 ItemsManagement.isPicked = false;
                 timePassed = 0f;
             }
