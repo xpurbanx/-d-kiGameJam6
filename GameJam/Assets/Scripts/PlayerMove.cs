0c1a22a1f750ea290c6789a946ed093e3d843219
MikeMG <michalek.mg@gmail.com>
2019-01-26 02:32:18 +0100

    Animacje 100% na czysto

--- GameJam/Assets/Scripts/PlayerMove.cs
+++ GameJam/Assets/Scripts/PlayerMove.cs
@@ -26,14 +26,29 @@ public class PlayerMove : MonoBehaviour
     void MoveHorizontally()
     {
         transform.Translate(horizontalValue * 0.001f * (speed * 2500f) * Time.deltaTime, 0, 0);
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(horizontalValue));
 
-
+        if (horizontalValue == 1f)
+            animator.SetBool("WalksRight", true);
+        else
+            animator.SetBool("WalksRight", false);
+        ///////////////////////////////////////
+        if (horizontalValue == -1f)
+            animator.SetBool("WalksLeft", true);
+        else
+            animator.SetBool("WalksLeft", false);
     }
 
     void MoveVertically()
     {
         transform.Translate(0, verticalValue * 0.001f * (speed * 2500f) * Time.deltaTime, 0);
-        animator.SetFloat("Speed", Mathf.Abs(verticalValue));
+        animator.SetFloat("VerticalSpeed", Mathf.Abs(verticalValue));
+
+        if (verticalValue == 1f)
+            animator.SetBool("WalksUp", true);
+        else
+            animator.SetBool("WalksUp", false);
+        ///////////////////////////////////////
         if (verticalValue == -1f)
             animator.SetBool("WalksDown", true);
         else
