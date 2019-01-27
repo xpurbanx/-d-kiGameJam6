4612a219aed3bb3d5f8b6d2f773da02b4d8a96e0
MikeMG <michalek.mg@gmail.com>
2019-01-27 03:09:40 +0100

    Aktualny, pare rzeczy do poprawki

--- GameJam/Assets/Scripts/ThrowItem.cs
+++ GameJam/Assets/Scripts/ThrowItem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ThrowItem : MonoBehaviour
 {
     //PUBLIC
+    public float howFar = 4.8f;
     public float speed = 10000.0f;
     public float step;
 
@@ -47,30 +48,59 @@ public class ThrowItem : MonoBehaviour
             RaycastHit2D hit;
 
             string name = s1.sprite.name;
+            string name2 = s2.sprite.name;
+
             if (name.Contains("down") || name.Contains("DOWN"))
             {
                 origin = new Vector2(transform.position.x, transform.position.y - 2f);
                 Debug.Log("down" + origin);
-                to = new Vector2(transform.position.x, transform.position.y - 3.5f);
+                to = new Vector2(transform.position.x, transform.position.y - howFar);
             }
             if (name.Contains("up") || name.Contains("UP"))
             {
                 Debug.Log("up");
-                to = new Vector2(transform.position.x, transform.position.y + 3.5f);
+                to = new Vector2(transform.position.x, transform.position.y + howFar);
                 origin = new Vector2(transform.position.x, transform.position.y + 2f);
             }
             if (name.Contains("left") || name.Contains("LEFT"))
             {
                 Debug.Log("left");
-                to = new Vector2(transform.position.x - 3.5f, transform.position.y);
+                to = new Vector2(transform.position.x - howFar, transform.position.y);
                 origin = new Vector2(transform.position.x - 2f, transform.position.y);
             }
             if (name.Contains("right") || name.Contains("RIGHT"))
             {
                 Debug.Log("right");
-                to = new Vector2(transform.position.x + 3.5f, transform.position.y);
+                to = new Vector2(transform.position.x + howFar, transform.position.y);
                 origin = new Vector2(transform.position.x + 2f, transform.position.y);
             }
+            
+
+            if (name2.Contains("down") || name2.Contains("DOWN"))
+            {
+                origin = new Vector2(transform.position.x, transform.position.y - 2f);
+                Debug.Log("down" + origin);
+                to = new Vector2(transform.position.x, transform.position.y - howFar);
+            }
+            if (name2.Contains("up") || name2.Contains("UP"))
+            {
+                Debug.Log("up");
+                to = new Vector2(transform.position.x, transform.position.y + howFar);
+                origin = new Vector2(transform.position.x, transform.position.y + 2f);
+            }
+            if (name2.Contains("left") || name2.Contains("LEFT"))
+            {
+                Debug.Log("left");
+                to = new Vector2(transform.position.x - howFar, transform.position.y);
+                origin = new Vector2(transform.position.x - 2f, transform.position.y);
+            }
+            if (name2.Contains("right") || name2.Contains("RIGHT"))
+            {
+                Debug.Log("right");
+                to = new Vector2(transform.position.x + howFar, transform.position.y);
+                origin = new Vector2(transform.position.x + 2f, transform.position.y);
+            }
+
             hit = Physics2D.Raycast(transform.position, origin, 3f);
 
             Debug.DrawRay(transform.position, origin, Color.red);
