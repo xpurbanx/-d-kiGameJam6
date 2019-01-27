8b1e86310b443446f07ff444701a574e96e64f8c
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2019-01-27 13:00:56 +0100

    Prefaby pierdolone

---
+++ GameJam/Assets/Scripts/HouseUpgrade.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseUpgrade : MonoBehaviour
+{
+    public List<GameObject> levels;
+    public int level_check;
+    public int currentLevel;
+   
+
+    void Start()
+    {
+
+        level_check = GetComponent<HouseState>().level;
+        foreach (var level in levels)
+            level.SetActive(false);
+    }
+
+    void UpgradeHouse()
+    {
+        for (int i = 0; i < levels.Capacity; i++)
+        {
+            if (level_check == i+1)
+            {
+                if (i == 8)
+                    levels[1].SetActive(false);
+
+                levels[i].SetActive(true);
+
+                
+
+                level_check++;
+                return;
+            }
+               
+                    
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentLevel = GetComponent<HouseState>().level;
+
+        if (level_check < currentLevel)
+            UpgradeHouse();
+
+
+    }
+}
