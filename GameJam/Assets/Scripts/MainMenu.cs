e088d8302d66dbd7ff3a6a04e64c9e61953601cf
Purban <xpurbanx@wp.pl>
2019-01-27 13:51:38 +0100

    Menu

---
+++ GameJam/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Ładowanie Sceny");
+        SceneManager.LoadScene("FinalScene");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Wyłączenie aplikacji");
+        Application.Quit();
+    }
+
+    public void RunCredits()
+    {
+        Debug.Log("Włączenie credits'ów");
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void RunReadme()
+    {
+        Debug.Log("Włączenie readme");
+        SceneManager.LoadScene("ReadmeScene");
+    }
+
+}
